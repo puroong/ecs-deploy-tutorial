4c3b4aab504411d64f9f9a153197eebac31bf8d9
nostalgia <skfk0135@gmail.com>
2021-01-10 01:08:48 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Main Page"
+
+@app.route("/health-check")
+def health_check():
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
