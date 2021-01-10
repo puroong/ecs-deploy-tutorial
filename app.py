b6a84b8655c1e9a1044a71168a4ab2e053e5af5c
ageun <skfk0135@gmail.com>
2021-01-10 17:01:33 +0900

    Ecs codedeploy (#1)

--- app.py
+++ app.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 def main():
     return "Main Page"
 
+@app.route("/new")
+def new():
+    return "new"
+
 @app.route("/health-check")
 def health_check():
     return "ok"
+
+@app.route("/new2")
+def new_2():
+    return "new2"
+
+@app.route("/new3")
+def new_3():
+    return "new3"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
